beb57a97087de3bce38b1c442f60852edca33a94
ajh <84565243+ajhNexcom@users.noreply.github.com>
2021-09-24 00:15:13 +0200

    final Draft

--- JsonFlatten/JsonHelper.cs
+++ JsonFlatten/JsonHelper.cs
@@ -1,8 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+using NJsonSchema.CodeGeneration.CSharp;
+using JsonSchema = NJsonSchema.JsonSchema;
 
 namespace JsonFlatten
 {
@@ -67,16 +77,14 @@ namespace JsonFlatten
                     if (char.IsDigit(cKey[^1]) && char.IsDigit(nKey[^1]) && cKey[^2] == '_' && nKey[^2] == '_')
                     {
                         sbValue.Append(cValue + ",");
-                        if (oDictioanry.ContainsKey(cKey))
-                        {
-                            oDictioanry.Remove(cKey);
-                            i--;
-                        }
+                        if (!oDictioanry.ContainsKey(cKey)) continue;
+                        oDictioanry.Remove(cKey);
+                        i--;
                     }
                     else
                     {
                         sbValue.Append(cValue);
-                        var mKey = cKey.Substring(0, cKey.Length - 2);
+                        var mKey = cKey[..^2];
                         nDictionary.TryAdd(mKey, sbValue.ToString());
                         sbValue.Clear();
                     }
@@ -89,5 +97,132 @@ namespace JsonFlatten
             }
             return nDictionary;
         }
+
+
+        public static bool IsValidJson(string input)
+        {
+            input = input.Trim();
+            try
+            {
+                // check if input is not null
+                if (string.IsNullOrWhiteSpace(input)) { return false; }
+                if ((input.StartsWith("{") && input.EndsWith("}")) || (input.StartsWith("[") && input.EndsWith("]"))) 
+                {
+                    try
+                    {
+                        // Parse as a Token
+                        var jToken = JToken.Parse(input);
+                        if (jToken.Type == JTokenType.Object)
+                        {
+                            //parse the input into a JObject
+                            var jObject = JObject.Parse(input);
+                            foreach (var jo in jObject)
+                            {
+                                var name = jo.Key;
+                                JToken value = jo.Value;
+                                switch (value)
+                                {
+                                    //if the element has a missing value, it will be Undefined - this is invalid
+                                    case {Type: JTokenType.Undefined}:
+                                        Console.WriteLine($"Undefined Value is not acceptable - {name}-{value}");
+                                        return false;
+                                    case {Type: JTokenType.Null}:
+                                        Console.WriteLine($"Null Value is not acceptable- - {name}-{value}");
+                                        return false;
+                                }
+                            }
+                        }
+
+                    }
+                    catch (JsonReaderException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        return false;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool IsStrictComplainceParse(string json)
+        {
+            try
+            {
+                // Throw an exception if the json string is not in strict compliance with the JSON standard by tokenize it with the JSON reader used by DataContractJsonSerializer:
+                using var stream = GenerateStreamFromString(json);
+                using var reader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max);
+                while (reader.Read())
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                // Wrap the XmlException in a JsonReaderException
+                throw new JsonReaderException($"Invalid property identifier character is not acceptable, only double quote acceptable:{ex}");
+            }
+            // Then actually parse with Json.NET
+             JToken.Parse(json);
+             return true;
+        }
+
+        static MemoryStream GenerateStreamFromString(string value)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(value ?? ""));
+        }
+
+
+
+        //public static async Task<bool> IsCheckMultipleErrors(string jsonString)
+        //{
+        //    //JsonSchema schemaFromFile = null;
+        //    try
+        //    {
+
+        //        //var settings = new JsonSerializerSettings { Error = (se, ev) => { ev.ErrorContext.Handled = true; } };
+        //        //dynamic d = JsonConvert.DeserializeObject(jsonString, settings);
+
+        //        //dynamic d = JsonConvert.DeserializeObject(jsonString);
+        //        //schemaFromFile = JsonSchema.FromSampleJson(jsonString);
+        //        //var classGenerator = new CSharpGenerator(schemaFromFile, new CSharpGeneratorSettings
+        //        //{
+        //        //    ClassStyle = CSharpClassStyle.Poco,
+        //        //});
+        //        //var codeFile = classGenerator.GenerateFile();
+
+        //        //var schema = JsonSchema.FromType<typeof()>();
+        //        //var schemaData = schema.ToJson();
+        //        // var errors = schema.Validate(jsonString);
+        //        //loading schema from a file
+        //        // schemaFromFile = JsonSchema.FromSampleJson(jsonString);
+        //        //var errors = schemaFromFile.ToJson();
+        //        //Validating the json file
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        //var errors = schemaFromFile.ToJson();
+        //    }
+
+        //    return true;
+        //}
+
+
+
+
     }
 }
